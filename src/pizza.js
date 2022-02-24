848f90836a9d5c2eea19a5f36961e323a46ccb2d
Alberto Aurensanz <aaurensanz@hiberus.com>
2022-02-24 21:15:44 +0100

    Changes

--- src/pizza.js
+++ src/pizza.js
@@ -7,22 +7,22 @@ const fileName = ['a_an_example.in.txt', 'b_better_start_small.in.txt', 'c_colla
 
 fileName.forEach(file => {
     const { contributors, projects } = prepareData(file)
-    _.orderBy(projects, ['scorer'], ['desc']);
+    const orderProjects = _.orderBy(projects, ['best', 'score', 'days'], ['asc', 'desc', 'asc']);
     const finalProjects = []
 
-    projects.forEach(project => {
+    orderProjects.forEach(project => {
         const finalProject = {name: project.name, people: []}
         project.skills.forEach(({ skill: projectSkill, level: levelSkill }) => {
             const person = contributors.find(({ skills, name, occupied }) => skills.find(({ skill, level }) => {
                 return skill === projectSkill && level >= levelSkill && !occupied
             }))
-            if (person) {
-                person.occupied = true
-                finalProject.people.push(person.name)
+            if (person && !finalProject.people.find(n => n === person)) {
+                finalProject.people.push(person)
             }
         })
         if (project.skills.length === finalProject.people.length) {
             finalProjects.push(finalProject)
+
         }
     })
 
